541eff3d84c21f01cf5217b9e42b4d3cfee37f32
guilherme130447 <pi.recicle@gmail.com>
2024-10-09 23:37:29 -0300

    solucao definitiva cors 2

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
-require('./routes')
-const { restoreSessions } = require('./sessions')
-const { routes } = require('./routes')
-const app = require('express')()
-const bodyParser = require('body-parser')
-const { maxAttachmentSize } = require('./config')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { maxAttachmentSize } = require('./config');
+const { restoreSessions } = require('./sessions');
+const { routes } = require('./routes');
 
-// Initialize Express app
-app.disable('x-powered-by')
-app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }))
-app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
-app.use('/', routes)
+const app = express();
 
-restoreSessions()
+// Middleware CORS
+const corsOptions = {
+  origin: 'http://localhost:3000', // ou a URL do front-end em produção
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'x-api-key'],
+  optionsSuccessStatus: 200,
+};
 
-module.exports = app
+app.use(cors(corsOptions)); // Aplica o middleware de CORS
+
+// Configurações do Body Parser
+app.disable('x-powered-by');
+app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }));
+app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }));
+
+// Definindo as rotas
+app.use('/', routes);
+
+// Restaurando sessões
+restoreSessions();
+
+module.exports =
